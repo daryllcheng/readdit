3b9299ed6a0e29e1bde56778659d456763ba4459
Daryll <daryllcsh@gmail.com>
2017-11-29 16:23:16 -0800

    Restructure app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore();
+
+ReactDOM.render(
+  <div>
+    <Provider store={ store }>
+      <App />
+    </Provider>
+  </div> , document.getElementById('root'));
+
 registerServiceWorker();
