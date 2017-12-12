0e90bdc37b407010030b90e4bf535ee5994844ce
Daryll <daryllcsh@gmail.com>
2017-12-12 08:48:56 -0800

    Fix deployed filename conversion discrepency

--- src/components/postsGrid/EmptyGrid.jsx
+++ src/components/postsGrid/EmptyGrid.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const EmptyGrid = () => (
   <div>
     <h2>Please select a few subreddits...</h2>
-    <img src="assets/emptySubreddits.png" alt="emptySubreddits" />
+    <img src="assets/emptysubreddits.png" alt="emptySubreddits" />
   </div>
 );
 
